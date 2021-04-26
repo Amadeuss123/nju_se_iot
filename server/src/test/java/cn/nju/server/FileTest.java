e23ab3f7814918264c8ed9d606db332b939d8c8c
bryantx <1207659084@qq.com>
2021-04-26 20:20:33 +0800

    suibian

--- server/src/test/java/cn/nju/server/FileTest.java
+++ server/src/test/java/cn/nju/server/FileTest.java
@@ -7,12 +7,34 @@ import org.springframework.util.ResourceUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileTest {
     @Test
     public void createFile() throws IOException {
+        float newBeam = 34F;
         File file = ResourceUtils.getFile("classpath:rules/sensorLight-beam-rule.drl");
         String content = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
         System.out.println(content);
+        System.out.println(replaceContent(newBeam, content));
+
     }
+    //beam  sound
+    private String replaceContent(float beam, String content){
+        String[] lines = content.split("\\r?\\n");
+        for(int i = 0; i < lines.length; i++){
+            if(lines[i].trim().startsWith("$protocol")){
+                int end = lines[i].indexOf('>');
+                lines[i] = lines[i].substring(0, end + 1) + " "+ beam + ")";
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String s : lines){
+            sb.append(s).append("\n");
+        }
+        return sb.toString();
+    }
+
+
 }
